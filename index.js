1858223fcffc86f9fd8ec6aca56a913bd356bacb
noor <noorridha028@gmail.com>
2022-12-06 10:59:11 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+const leastFavoriteCustomer = 'value';
+
+function upperCaseCustomerName() {
+  customerName = 'BOB';
+};
+
+// upperCaseCustomerName();
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+};
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+};
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new value';
+};
